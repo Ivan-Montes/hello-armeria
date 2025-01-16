fca711a731d623e27fd5ec7c4956e2790dd26c31
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-01-16 07:40:00 +0100

    feat: add Armeria configuration

---
+++ grpc-service/src/main/java/dev/ime/config/ArmeriaConfiguration.java
@@ -0,0 +1,34 @@
+package dev.ime.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.linecorp.armeria.server.grpc.GrpcService;
+import com.linecorp.armeria.server.logging.LoggingService;
+import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
+
+import dev.ime.error.GrpcExceptionHandler;
+import dev.ime.service.TreeServiceImpl;
+import io.grpc.protobuf.services.ProtoReflectionService;
+
+@Configuration
+public class ArmeriaConfiguration {
+
+	@Bean
+	ArmeriaServerConfigurator armeriaServerConfigurator(TreeServiceImpl treeServiceImpl, GrpcExceptionHandler grpcExceptionHandler) {
+	   
+		final GrpcService grpcService =
+                GrpcService.builder()
+                           .addService(treeServiceImpl)
+                           .addService(ProtoReflectionService.newInstance())
+                           .enableUnframedRequests(true)                        
+                           .exceptionHandler(grpcExceptionHandler)
+                           .useBlockingTaskExecutor(true)
+                           .build();
+		
+		return serverBuilder ->  serverBuilder.service(grpcService,
+		           LoggingService.newDecorator())
+				;
+	}
+	
+}
