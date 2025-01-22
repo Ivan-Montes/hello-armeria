f7bf312e6e85662ef8b314a8329374b98b6e6e8c
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-01-22 07:36:38 +0100

    feat: initial commit grpc-service-transcoding

---
+++ grpc-service-transcoding/src/main/java/dev/ime/GrpcServiceTranscodingApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServiceTranscodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcServiceTranscodingApplication.class, args);
+	}
+
+}
